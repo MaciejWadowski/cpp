863f84b3c6a675c1d5a50a7aebbefecb653e0ec5
maciej <macwadowski@tlen.pl>
2018-03-23 20:18:27 +0100

    greatest product with 2 steps.

--- JIMP_AI/lab_2/greatestproduct/GreatestProduct.h
+++ JIMP_AI/lab_2/greatestproduct/GreatestProduct.h
@@ -8,6 +8,8 @@
 #include <vector>
 
 int GreatestProduct(const std::vector<int> &numbers, int k);
-
+void ResultTab(int result_elements[], const std::vector<int> &numbers, int k);
+void ResultTabv2(int result_elements[], const std::vector<int> &numbers, int k);
+int abs(int val);
 
 #endif //JIMP_EXERCISES_GREATESTPRODUCT_H
--- JIMP_AI/lab_2/greatestproduct/main.cpp
+++ JIMP_AI/lab_2/greatestproduct/main.cpp
@@ -5,7 +5,7 @@
 
 int main(){
     using namespace std;
-    vector<int> tab= {1,3,4,5,1,23,2,31};
+    vector<int> tab= {-11, 8, 2, 9, -9, 8, 5, -1,};
     int result;
     int k = 2;
     result = GreatestProduct(tab, k);

e3c5ef348704ea3855b0eb1f66c6d751067e66f0
maciej <macwadowski@tlen.pl>
2018-04-01 23:32:30 +0200

    Ccounter works.

--- JIMP_AI/lab_3/ccounter/CCounter.cpp
+++ JIMP_AI/lab_3/ccounter/CCounter.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "CCounter.h"
+
+using std::unique_ptr;
+using std::string;
+using std::make_unique;
+using std::pair;
+using std::map;
+
+namespace ccounter{
+
+    unique_ptr<Counter> Init(){
+        auto counter = make_unique<Counter>();
+        return counter;
+    }
+
+    void Inc(string key, unique_ptr<Counter> *counter){
+        pair <string, int> new_element ={key, 1};
+        map <string, int>::iterator it;
+
+        if((*counter)->data.empty()) (*counter)->data.insert(new_element);
+
+        else{
+            it = (*counter)->data.find(key);
+            if(it != (*counter)->data.end()) it->second++;
+            else (*counter)->data.insert(new_element);
+        }
+    }
+
+    int Counts(const unique_ptr<Counter> &counter, string key){
+        map<string, int>::iterator it;
+        it = counter->data.find(key);
+
+        if(it != counter->data.end()) return it->second;
+        else return 0;
+    }
+
+    void SetCountsTo(string key, int value, unique_ptr<Counter> *counter){
+        map<string, int>::iterator it;
+        it = (*counter)->data.find(key);
+        if(it != (*counter)->data.end()) it->second = value;
+    }
+}
--- JIMP_AI/lab_3/ccounter/main.cpp
+++ JIMP_AI/lab_3/ccounter/main.cpp
@@ -2,3 +2,25 @@
 // Created by maciej on 31.03.18.
 //
 
+#include "CCounter.h"
+
+using ccounter::Inc;
+using std::cout;
+using std::endl;
+using ccounter::Counts;
+
+int main(){
+    std::unique_ptr<ccounter::Counter> counter = ccounter::Init();
+    Inc("a", &counter);
+    Inc("b", &counter);
+    Inc("c", &counter);
+    Inc("a", &counter);
+    Inc("b", &counter);
+    Inc("a", &counter);
+    Inc("XDD", &counter);
+    cout << Counts(counter, "a") << "->a " << Counts(counter, "b") << "->b "
+    << Counts(counter, "XDD") << "->XDD " << Counts(counter, "nothing") << "->nothing" << endl;
+    ccounter::SetCountsTo("c", 10, &counter);
+    cout << Counts(counter, "c") << endl;
+    return 0;
+}

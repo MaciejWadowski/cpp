440d4523aaceffbcb94a4b0657636a9e1e56bc46
Wadziuxxx <wadziuxxx@wadziuxxx.maciej>
2018-04-29 19:17:10 +0200

    StudentRepository works

--- JIMP_AI/lab_6/academia/StudentRepository.cpp
+++ JIMP_AI/lab_6/academia/StudentRepository.cpp
@@ -14,16 +14,24 @@ academia::StudentRepository::StudentRepository(std::initializer_list<academia::S
         student.push_back(i);
 }
 
-academia::Student academia::StudentRepository::operator[](std::string id_) const {
-    for(auto i : student)
+academia::Student &academia::StudentRepository::operator[](std::string id_) {
+    for(auto &i : student)
         if (id_ == i.Id()) return i;
-    return Student{};
+    Student st{};
+    return st;
 }
 
 bool academia::StudentRepository::operator==(const academia::StudentRepository &o1) const {
     if(student.size() != o1.student.size()) return false;
-    for(int i = 0; i < student.size();i++)
-        if(student != o1.student) return false;
+    if(this == &o1) return true;
+    for(auto s1 : student){
+        for(auto s2 : o1.student){
+            if(s1.id == s2.id){
+                if(s1 == s2) break;
+                else return false;
+            }
+        }
+    }
     return true;
 }
 
@@ -44,6 +52,7 @@ academia::Student::Student(std::string id_, std::string first_name_, std::string
 }
 
 bool academia::Student::operator==(const academia::Student &o1) const {
+    if(this == &o1) return true;
     return first_name==o1.first_name && last_name == o1.last_name
             && id == o1.id && program == o1.program && studyYear == o1.studyYear;
 }
@@ -57,4 +66,51 @@ bool academia::Student::operator!=(const academia::Student &o1) const {
     return false;
 }
 
+std::string academia::Student::Id() const {
+    return id;
+}
 
+std::string academia::Student::FirstName() const {
+    return first_name;
+}
+
+std::string academia::Student::LastName() const {
+    return last_name;
+}
+std::string academia::Student::Program() const {
+    return program;
+}
+
+academia::StudyYear academia::Student::Year() const {
+    return studyYear;
+}
+
+void academia::Student::ChangeLastName(std::string last_name_){
+    last_name.clear();
+    last_name = last_name_;
+}
+
+
+academia::StudyYear::StudyYear() : data(1){}
+academia::StudyYear::StudyYear(int data_):data(data_){}
+
+bool academia::StudyYear::operator==(const academia::StudyYear &o1) const {
+    return data == o1.data;
+}
+
+bool academia::StudyYear::operator<(const academia::StudyYear &o1) const {
+    return data < o1.data;
+}
+
+academia::StudyYear::operator int() {
+    return data;
+}
+
+int academia::StudyYear::getStudyYear() const {
+    return data;
+}
+
+
+bool academia::operator==(const int value, const academia::StudyYear &o1) {
+    return value == o1.getStudyYear();
+}
--- JIMP_AI/lab_6/academia/StudentRepository.h
+++ JIMP_AI/lab_6/academia/StudentRepository.h
@@ -8,32 +8,32 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <type_traits>
 
 namespace academia{
     class StudyYear{
     public:
         friend class Student;
-        StudyYear(){data = 1;};
-        explicit StudyYear(int data_){data = data_;};
-        bool operator == (const StudyYear &o1)const{return data == o1.data;};
-        bool operator < (const StudyYear &o1) const{return data < o1.data;};
-        operator int(){return data;};
-        int getStudyYear()const{return data;}
+        StudyYear();
+        explicit StudyYear(int data_);
+        bool operator == (const StudyYear &o1)const;
+        bool operator < (const StudyYear &o1) const;
+        operator int();
+        int getStudyYear()const;
     private:
         int data;
     };
-    bool operator==(const int value, const StudyYear &o1){return value == o1.getStudyYear();};
+    inline bool operator==(const int value, const StudyYear &o1);
     class Student{
     public:
+        friend class StudentRepository;
         Student();
         Student(std::string id_, std::string first_name_,std::string last_name_, std::string program_, int studyYear_);
-        std::string Id()const{return id;};
-        std::string FirstName()const{return first_name;};
-        std::string LastName()const{return last_name;};
-        std::string Program()const{return program;};
-        StudyYear Year()const{return studyYear;};
-        void ChangeLastName(std::string last_name_){last_name = last_name_;};
+        std::string Id()const;
+        std::string FirstName()const;
+        std::string LastName()const;
+        std::string Program()const;
+        StudyYear Year()const;
+        void ChangeLastName(std::string last_name_);
         bool operator == (const Student &o1) const;
         bool operator != (const Student &o1) const;
     private:
@@ -50,7 +50,7 @@ namespace academia{
         StudentRepository() = default;
         StudentRepository(std::initializer_list<Student> student_list);
         int StudentCount()const;
-        Student operator[](std::string id_)const;
+        Student &operator[](std::string id_);
         bool operator == (const StudentRepository &o1)const;
     private:
         std::vector<Student> student;
